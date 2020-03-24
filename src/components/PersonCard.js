444922451da0ad464cec5f168426d847503930b6
Michelle Tanzil <mtanzil01@gmail.com>
2020-03-23 18:27:17 -0700

    completed putting it together assignment

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,5 +1,21 @@
 import React, { Component } from "react";
+  const Button = (props) => {
+    return (
+      <button onClick={props.handleClick}>Birthday Button for {props.firstName} {props.lastName}</button>
+    )
+  }
 class PersonCardComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      updatedAge: this.props.age
+    }
+  }
+  handleClick = () => {
+    this.setState({
+      updatedAge: this.state.updatedAge + 1
+    })
+  }
   render() {
     const { lastName, firstName, age, hairColor } = this.props;
     return (
@@ -7,8 +23,9 @@ class PersonCardComponent extends Component {
         <h1>
           {lastName}, {firstName}
         </h1>
-        <p>Age: {age}</p>
+        <p>Age: {this.state.updatedAge}</p>
         <p>Hair Color: {hairColor}</p>
+        <Button handleClick={this.handleClick} firstName={this.props.firstName} lastName={this.props.lastName}/>
       </div>
     );
   }
